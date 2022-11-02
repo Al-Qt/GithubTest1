4c28fb122c14ba355c36c350807a67f383dd1d3f
Al-Qt <1798052405@qq.com>
2022-11-02 23:14:51 +0800

    添加了一个注释

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world");          //输出
+        System.out.println("Hello world again~");   //输出
     }
 }
